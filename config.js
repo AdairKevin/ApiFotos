88a757a07f80b4717f5369e66dd6897b622f8d21
Adair <quesuabe@gmail.com>
2025-02-11 21:42:13 -0600

    cambios

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+import { config } from "dotenv";
+
+config();
+
 export const MONGODB_URI = process.env.MONGODB_URI;
 
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
@@ -5,3 +9,5 @@ export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+
+export const PORT = process.env.PORT || 3000;
