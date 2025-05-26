1c7ef5d332dfa9caf4ee329251138531fa8ee4f8
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2025-05-26 13:03:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectToDB } from "./utils/mongoose.js";
 
-// database/connectToDB.js
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
-
 async function main() {
   await connectToDB();
 
