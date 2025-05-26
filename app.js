0988acbc7975cc6e7675ad6881e91cbae5226e24
Adair Kevin <101688536+AdairKevin@users.noreply.github.com>
2025-05-26 15:00:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ import fotosRouter from "./routes/fotos.routes.js";
 
 import morgan from "morgan";
 
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "https://albumdigital-fotografico.netlify.app", // tu dominio en Netlify
+  methods: ["GET", "POST"],
+  credentials: true,
+};
+
 const app = express();
 
 app.use(morgan("dev"));
@@ -13,13 +21,6 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use(fotosRouter);
 
+app.use(cors(corsOptions));
+
 export default app;
-
-const whitelist = ["https://albumdigital-fotografico.netlify.app"];
-
-app.use(
-  cors({
-    origin: whitelist,
-    credentials: true,
-  })
-);
